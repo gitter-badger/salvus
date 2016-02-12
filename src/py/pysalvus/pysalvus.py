2bf1e4e0289cac6988dc6fa2dfc0d4204fbb2097
Lion Krischer <krischer@geophysik.uni-muenchen.de>
2016-02-12 11:57:47 +0100

    Raising errors and cleaning python code.

--- src/py/pysalvus/pysalvus.py
+++ src/py/pysalvus/pysalvus.py
@@ -1,4 +1,5 @@
 #! -*- coding: utf-8 -*-
+from __future__ import print_function
 
 import click
 import numpy as np
@@ -6,39 +7,50 @@ import numpy as np
 from code_generation import code_generator
 from model_handling import model
 
+
 @click.group()
 def code_generation():
     """Test."""
-    pass
+    raise NotImplementedError
+
 
 @code_generation.command()
-@click.option('--dimension', type=click.Choice(['2', '3']), required=True, help='Dimension of element.')
-@click.option('--polynomial_order', required=True, help='Lagrange polynomial order.')
+@click.option('--dimension', type=click.Choice(['2', '3']), required=True,
+              help='Dimension of element.')
+@click.option('--polynomial_order', required=True,
+              help='Lagrange polynomial order.')
 def HyperCube_generate_gll_basis(dimension, polynomial_order):
-    """Autogenerate the appropriate code for a tensorized gll basis on a 2D/3D hypercube element.
+    """
+    Autogenerate the appropriate code for a tensorized gll basis on a 2D/3D
+    hypercube element.
 
     :param dimension: Dimension of element.
     :param polynomial_order: Lagrange polynomial order.
     """
-
     if dimension == '2':
         code_generator.tensorized_basis_2D(int(polynomial_order))
     else:
-        pass
+        raise NotImplementedError
+
 
 @click.group()
 def model_handling():
-    """Test."""
-    pass
+    """
+    Test.
+    """
+    raise NotImplementedError
+
 
 @model_handling.command()
-@click.option('--input_file', help='Exodus file to add parameter to', type=click.Path(readable=True), required=True)
+@click.option('--input_file', help='Exodus file to add parameter to',
+              type=click.Path(readable=True), required=True)
 @click.option('--name', help='Name of parameter.', default='Velocity')
 @click.option('--value', help='Value of constant parameter.', default=4.0)
 @click.option('--output_file', help='Exodus file to to', default=None)
 def add_constant_material_parameter(input_file, name, value, output_file):
-    '''Test.'''
-
+    '''
+    Test.
+    '''
     working_model = model.ExodusModel(exodus_file=input_file)
     working_model.readFromExodus()
 
@@ -50,10 +62,15 @@ def add_constant_material_parameter(input_file, name, value, output_file):
 
 @click.group()
 def solver_operation():
-    """Test."""
-    pass
+    """
+    Test.
+    """
+    raise NotImplementedError
+
 
 @click.group()
 def optimization_tools():
-    """Test."""
-    pass
+    """
+    Test.
+    """
+    raise NotImplementedError
